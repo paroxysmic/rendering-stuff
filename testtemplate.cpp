c4660027a2bfbf5fe6aeca88df1c92c7e05a6e3f
paroxysmic <masni28@oakwoodstudents.org>
2025-06-10 20:06:37 -0700

    updated test template to be more c++ like

--- testtemplate.cpp
+++ testtemplate.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <array>
 #include <time.h>
 #include "mlib.h"
-int iw = 400;
-int ih = 400;
-int main() {
-    long scrarr[iw * ih];
-    std::cout << "P3\n" << iw << '\n' << ih << "\n" << 255 << '\n';
-    for (int x = 0; x < iw; x++) {
+const int iw = 99;
+const int ih = 400;
+int main(int argc, char **argv) {
+    if(argc == 2){
+        std::ofstream out(argv[1], std::ios::binary);
+        std::vector<long> scrarr;
+        for(int i=0;i<iw*ih;i++){
+            scrarr.push_back(0x00ff00);
+        }
+        std::string header = "P6\n" + std::to_string(iw) + "\n" + std::to_string(ih) + "\n255\n";
+        out.write(header.c_str(), header.size());
+        clock_t t = clock();
+        //do your things here
+        t = clock() - t;
+        std::cerr << "done with writing to arr!\n";
+        std::cerr << "took " << t << "ticks, or " << (float)t / 1000 << "seconds\n";
+        std::vector<char> carr;
         for (int y = 0; y < ih; y++) {
-            setpix(vec2(x, y), iw, ih, scrarr, 0x000000);
+            for (int x = 0; x < iw; x++) {
+                long cell = scrarr[x + y * iw];
+                carr.push_back((cell & 0xff0000) >> 16);
+                carr.push_back((cell & 0x00ff00) >> 8);
+                carr.push_back(cell & 0x0000ff);
+            }
         }
+        out.write(carr.data(), carr.size());
+        std::cerr << "done writing to file!";
     }
-    clock_t t = clock();
-    //do your things here
-    t = clock() - t;
-    std::cerr << "done with writing to arr!\n";
-    std::cerr << "took " << t << "ticks, or " << (float)t / 1000 << "seconds\n";
-    for (int y = 0; y < ih; y++) {
-        for (int x = 0; x < iw; x++) {
-            long col = scrarr[x + y * iw];
-            char r = (col & 0xff0000) >> 16;
-            char g = (col & 0x00ff00) >> 8;
-            char b = (col & 0x0000ff);
-            std::cout << r << ' ' << g << ' ' << b << '\n';
-        }
+    else {
+        std::cerr << "submit the filename after!";
     }
-    std::cerr << "done writing to file!";
     return 0;
 }
